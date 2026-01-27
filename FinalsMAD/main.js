5411264baf461a1de80798f49233aaecb975095b
Jimuel <crimsonrush00@gmail.com>
2026-01-27 11:51:53 +0800

    Add files via upload

--- FinalsMAD/main.js
+++ FinalsMAD/main.js
@@ -13,7 +13,7 @@ camera.position.z = 12;
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(0x000000, 0); // transparent
+renderer.setClearColor(0x000000, 0); 
 
 scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 
@@ -21,14 +21,19 @@ const light = new THREE.PointLight(0xffffff, 1);
 light.position.set(10, 10, 10);
 scene.add(light);
 
-const colors = [0x0085c7, 0xf4c300, 0x000000, 0x009f3d, 0xdf0024];
+const isMobile = window.innerWidth < 768;
+
+const colors = isMobile
+  ? [0x0085c7, 0xf4c300, 0x009f3d]
+  : [0x0085c7, 0xf4c300, 0x000000, 0x009f3d, 0xdf0024];
+
 const rings = [];
 
 colors.forEach((color, i) => {
   const geometry = new THREE.TorusGeometry(1.5, 0.2, 16, 100);
   const material = new THREE.MeshStandardMaterial({ color });
   const ring = new THREE.Mesh(geometry, material);
-  ring.position.x = (i - 2) * 3;
+  ring.position.x = (i - (colors.length - 1) / 2) * 3;
   rings.push(ring);
   scene.add(ring);
 });
@@ -49,4 +54,3 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
